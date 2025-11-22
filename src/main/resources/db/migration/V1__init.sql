a844bf3935a8b4267733a398b55b2c9de69e46f9
mukhammadjonkholmukhamedov <kh.muxammadjon2@gmail.com>
2025-11-22 17:41:48 +0500

    add: migration and tables

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,40 @@
+-- DB Merchants:
+-- - id
+-- - name
+-- - webhook
+-- - tax-number
+-- - created-at
+-- - login
+-- -password
+create table merchants
+(
+    id        bigint generated always as identity primary key,
+    name      varchar(50)  not null,
+    webhook   varchar(100) not null,
+    tax_number varchar(50)  not null unique,
+    created_at timestamp default current_timestamp,
+    login     varchar(50)  not null unique,
+    password  varchar(255) not null
+);
+
+-- DB Notifications:
+-- - id
+-- - status (CREATED, SENT, FAILED)
+-- - created-at
+-- - updated-at
+-- - content
+-- - merchant-id
+-- - receiver
+create type stats as enum ('CREATED','SENT','FAILED');
+
+create table notifications
+(
+    id         bigint generated always as identity primary key,
+    status     stats        not null default 'CREATED',
+    created_at  timestamp             default current_timestamp,
+    updated_at  timestamp             default current_timestamp,
+    content    varchar(255) not null,
+    merchant_id bigint       not null,
+    receiver   varchar(50)  not null,
+    constraint fk_merchant foreign key (merchant_id) references merchants (id) on delete cascade
+);
