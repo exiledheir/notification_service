0de9f0fbac1d66139a13dff635bc71e284fa85b4
mukhammadjonkholmukhamedov <kh.muxammadjon2@gmail.com>
2025-11-22 17:16:15 +0500

    init

---
+++ src/test/java/uz/mukhammadjon/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.mukhammadjon.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
